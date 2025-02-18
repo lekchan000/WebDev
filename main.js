c514ec22bc1c7b45f23b3c1503b9ecbc593a56fc
lekchan000 <lekchan000@gmail.com>
2025-02-18 11:04:54 +0700

    Add main.js with variable declarations and link to test.html

---
+++ main.js
@@ -0,0 +1,27 @@
+/* 
+// string , number , boolean , array , object
+// 1. string 
+let firstname = "Admiral Yao"
+
+// 2. number
+let age = 18
+let height = 160
+
+// 3. boolean
+let isThai = true
+
+// 5. object
+let students = [{
+    name : "Rio" ,
+    age : 16 ,
+    grade : "A"
+}, {
+    name : "Yuka" ,
+    age : 16 ,
+    grade : "B"
+}]
+
+console.log("My name is ",firstname)
+console.log(students) 
+*/
+
