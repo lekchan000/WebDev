168c17919767c81656e9a6dcfe1aa51b2a80c945
lekchan000 <lekchan000@gmail.com>
2025-02-20 14:46:04 +0700

    Refactor submitData function in index.js to collect user input and fix input field name in register.html

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 function submitData() {
-    console.log("Submit Data")
+    let firstname_DOM = document.querySelector('input[name = firstname]')
+    let lastname_DOM = document.querySelector('input[name = lastname]')
+    let age_DOM = document.querySelector('input[name = age]')
+    let gender_DOM = document.querySelector('input[name = gender]:checked')
+    let interest_DOM = document.querySelectorAll('input[name=Interest]:checked')
+        let interest = ''
+        for (let i = 0 ; i < interest_DOM.length; i++) {
+            interest += interest_DOM.value + ', '
+        }
+    let description_DOM = document.querySelector('textarea[name=description]')
+
+    //---------------------------------------------------------------------------
+    let userData = {
+        firstname: firstname_DOM.value ,
+        lastname: lastname_DOM.value ,
+        age: age_DOM.value ,
+        gender: gender_DOM.value ,
+        interest: interest,
+        description: description_DOM.value
+    }
+    console.log('Submit Data',userData)
 }
